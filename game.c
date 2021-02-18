7b2692348a7862aa0119d7f7f3f63f159f994514
agustafson19 <arvidg@iastate.edu>
2021-02-18 17:02:36 -0600

    Reworked Saving and Loading
    Reworked saving and loading to be complient with standards; temporarily
    removed corridors.

--- game.c
+++ game.c
@@ -8,7 +8,7 @@
 #include <endian.h>
 
 #define W_WIDTH 80
-#define W_HEIGHT 24
+#define W_HEIGHT 21
 
 #define SAVE 0x01
 #define LOAD 0x02
@@ -26,64 +26,70 @@ typedef struct entity {
     char symbol;
 } entity_t;
 
-void getArgs(int argc, char *argv[], uint8_t *args);
+void get_args(int argc, char *argv[], uint8_t *args);
 
-void createrooms(room_t **rooms, uint8_t *numrooms);
+void generate(entity_t **player, uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t *num_rooms, room_t **rooms, uint16_t *num_u_stairs, entity_t **u_stairs, uint16_t *num_d_stairs, entity_t **d_stairs);
 
-void save(room_t *rooms, uint8_t numrooms, entity_t *player);
-void load(room_t **rooms, uint8_t *numrooms, entity_t **player);
+void save(entity_t *player, uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t num_rooms, room_t *rooms, uint16_t num_u_stairs, entity_t *u_stairs, uint16_t num_d_stairs, entity_t *d_stairs);
+void load(entity_t **player, uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t *num_rooms, room_t **rooms, uint16_t *num_u_stairs, entity_t **u_stairs, uint16_t *num_d_stairs, entity_t **d_stairs);
 
-void maprooms(char map[W_HEIGHT][W_WIDTH], room_t *rooms, uint8_t numrooms);
+void map_rooms(char map[W_HEIGHT][W_WIDTH], uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t num_rooms, room_t *rooms);
 
-void initmap(char map[W_HEIGHT][W_WIDTH]);
-void sketchmap(char display[W_HEIGHT][W_WIDTH], char map[W_HEIGHT][W_WIDTH]);
+void init_map(char map[W_HEIGHT][W_WIDTH]);
+void sketch_map(char display[W_HEIGHT][W_WIDTH], char map[W_HEIGHT][W_WIDTH]);
 
-void place(char display[W_HEIGHT][W_WIDTH], entity_t *entities, uint8_t count);
+void place(char display[W_HEIGHT][W_WIDTH], entity_t *entities, uint16_t count);
 void draw(char display[W_HEIGHT][W_WIDTH]);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     /* Switch Processing */
     uint8_t args = 0x00;
-    getArgs(argc, argv, &args);
+    get_args(argc, argv, &args);
 
     /* Seeding */
     srand(time(NULL));
 
     /* Declarations */
-    uint8_t numrooms;
-    room_t *rooms;
     entity_t *player;
+    uint8_t hardness[W_HEIGHT][W_WIDTH];
+    uint16_t num_rooms;
+    room_t *rooms;
+    uint16_t num_u_stairs;
+    entity_t *u_stairs;
+    uint16_t num_d_stairs;
+    entity_t *d_stairs;
     char map[W_HEIGHT][W_WIDTH];
     char display[W_HEIGHT][W_WIDTH];
 
     /* Saving and Loading */
     if (args & LOAD)
-        load(&rooms, &numrooms, &player);
-    else {
-        createrooms(&rooms, &numrooms);
-        player = malloc(sizeof(entity_t));
-        player->xpos = rooms[0].xpos;
-        player->ypos = rooms[0].ypos;
-        player->symbol = '@';
-    }
+        load(&player, hardness, &num_rooms, &rooms, &num_u_stairs, &u_stairs, &num_d_stairs, &d_stairs);
+    else
+        generate(&player, hardness, &num_rooms, &rooms, &num_u_stairs, &u_stairs, &num_d_stairs, &d_stairs);
     if (args & SAVE)
-        save(rooms, numrooms, player);
+        save(player, hardness, num_rooms, rooms, num_u_stairs, u_stairs, num_d_stairs, d_stairs);
 
     /* Sketching Map */
-    initmap(map);
-    maprooms(map, rooms, numrooms);
+    init_map(map);
+    map_rooms(map, hardness, num_rooms, rooms);
 
     /* Displaying */
-    sketchmap(display, map);
+    sketch_map(display, map);
     place(display, player, 1);
+    place(display, u_stairs, num_u_stairs);
+    place(display, d_stairs, num_d_stairs);
     draw(display);
 
     /* Cleaning Memory */
     free(player);
     free(rooms);
+    free(u_stairs);
+    free(d_stairs);
 }
 
-void getArgs(int argc, char *argv[], uint8_t *args) {
+void get_args(int argc, char *argv[], uint8_t *args)
+{
     uint8_t i;
     for (i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "--save")) {
@@ -95,20 +101,21 @@ void getArgs(int argc, char *argv[], uint8_t *args) {
     }
 }
 
-void createrooms(room_t **rooms, uint8_t *numrooms) {
+void generate(entity_t **player, uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t *num_rooms, room_t **rooms, uint16_t *num_u_stairs, entity_t **u_stairs, uint16_t *num_d_stairs, entity_t **d_stairs)
+{
     /* printf("CREATING:\n"); */
     uint8_t i, j, v;
-    *numrooms = 6 + rand() % 2;
-    *rooms = malloc(sizeof(room_t) * *numrooms);
+    *num_rooms = 6 + rand() % 2;
+    *rooms = malloc(sizeof(room_t) * *num_rooms);
     do {
-        for (i = 0; i < *numrooms; i++) {
+        for (i = 0; i < *num_rooms; i++) {
             (*rooms)[i].xsize = 4 + rand() % 10;
             (*rooms)[i].ysize = 3 + rand() % 10;
             (*rooms)[i].xpos = rand() % (W_WIDTH - (*rooms)[i].xsize);
             (*rooms)[i].ypos = rand() % (W_HEIGHT - (*rooms)[i].ysize);
         }
         v = 1;
-        for (i = 1; i < *numrooms && v; i++) {
+        for (i = 1; i < *num_rooms && v; i++) {
             for (j = 0; j < i && v; j++) {
                 if (!((*rooms)[i].xpos + (*rooms)[i].xsize < (*rooms)[j].xpos || (*rooms)[j].xpos + (*rooms)[j].xsize < (*rooms)[i].xpos)
                         && !((*rooms)[i].ypos + (*rooms)[i].ysize < (*rooms)[j].ypos || (*rooms)[j].ypos + (*rooms)[j].ysize < (*rooms)[i].ypos)) {
@@ -118,78 +125,162 @@ void createrooms(room_t **rooms, uint8_t *numrooms) {
         }
     } while(!v);
     /*
-    for (i = 0; i < *numrooms; i++) {
+    for (i = 0; i < *num_rooms; i++) {
         printf("%d : %d : %d : %d\n", (*rooms)[i].xpos, (*rooms)[i].ypos, (*rooms)[i].xsize, (*rooms)[i].ysize);
     }
     */
+    *player = malloc(sizeof(entity_t));
+    (*player)->xpos = (*rooms)[0].xpos;
+    (*player)->ypos = (*rooms)[0].ypos;
+    (*player)->symbol = '@';
+    *num_u_stairs = 1;
+    *u_stairs = malloc(sizeof(entity_t));
+    (*u_stairs)->xpos = (*rooms)[2].xpos + 1;
+    (*u_stairs)->ypos = (*rooms)[2].ypos + 1;
+    (*u_stairs)->symbol = '<';
+    *num_d_stairs = 1;
+    *d_stairs = malloc(sizeof(entity_t));
+    (*d_stairs)->xpos = (*rooms)[*num_rooms - 1].xpos + 1;
+    (*d_stairs)->ypos = (*rooms)[*num_rooms - 1].ypos + 1;
+    (*d_stairs)->symbol = '>';
+    //TODO HARDNESS
 }
 
-void save(room_t *rooms, uint8_t numrooms, entity_t *player) {
+void save(entity_t *player, uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t num_rooms, room_t *rooms, uint16_t num_u_stairs, entity_t *u_stairs, uint16_t num_d_stairs, entity_t *d_stairs)
+{
     /* printf("SAVING:\n"); */
-    uint8_t i;
-    uint16_t ws;
+    uint8_t i, j;
+    uint8_t w_byte;
+    uint16_t w_short;
     uint32_t w;
+    char version[13] = "RLG327-@2021";
     char *home = getenv("HOME");
     char *gamedir = ".rlg327";
     char *save = "dungeon";
     char *path = malloc(strlen(home) + strlen(gamedir) + strlen(save) + 3);
     sprintf(path, "%s/%s/%s", home, gamedir, save);
     FILE *f = fopen(path, "w");
-    fwrite(&numrooms, sizeof(uint8_t), 1, f);
-    for (i = 0; i < numrooms; i++) {
-        w = (rooms[i].xpos << 24) + (rooms[i].ypos << 16) + (rooms[i].xsize << 8) + rooms[i].ysize;
-        /* printf("%x\n", w); */
-        w = htobe32(w);
-        fwrite(&w, sizeof(uint32_t), 1, f);
-        /* printf("%d : %d : %d : %d\n", rooms[i].xpos, rooms[i].ypos, rooms[i].xsize, rooms[i].ysize); */
+    for (i = 0; i < 12; i++) {
+        fwrite(&version[i], sizeof(char), 1, f);
     }
-    ws = (player->xpos << 8) + player->ypos;
-    /* printf("%x\n", ws); */
-    ws = htobe16(ws);
-    fwrite(&ws, sizeof(uint16_t), 1, f);
+    w = htobe32(0);
+    fwrite(&w, sizeof(uint32_t), 1, f);
+    w = htobe32(1708 + num_rooms * 4 + num_u_stairs * 2 + num_d_stairs * 2);
+    fwrite(&w, sizeof(uint32_t), 1, f);
+    w_byte = player->xpos;
+    fwrite(&w_byte, sizeof(uint8_t), 1, f);
+    w_byte = player->ypos;
+    fwrite(&w_byte, sizeof(uint8_t), 1, f);
+    for (i = 0; i < W_HEIGHT; i++) {
+        for (j = 0; j < W_WIDTH; j++) {
+            w_byte = hardness[i][j];
+            fwrite(&w_byte, sizeof(uint8_t), 1, f);
+        }
+    }
+    w_short = htobe16(num_rooms);
+    fwrite(&w_short, sizeof(uint16_t), 1, f);
+    for (i = 0; i < num_rooms; i++) {
+        w_byte = rooms[i].xpos;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+        w_byte = rooms[i].ypos;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+        w_byte = rooms[i].xsize;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+        w_byte = rooms[i].ysize;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+    }
+    w_short = htobe16(num_u_stairs);
+    fwrite(&w_short, sizeof(uint16_t), 1, f);
+    for (i = 0; i < num_u_stairs; i++) {
+        w_byte = u_stairs[i].xpos;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+        w_byte = u_stairs[i].ypos;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+    }
+    w_short = htobe16(num_d_stairs);
+    fwrite(&w_short, sizeof(uint16_t), 1, f);
+    for (i = 0; i < num_d_stairs; i++) {
+        w_byte = d_stairs[i].xpos;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+        w_byte = d_stairs[i].ypos;
+        fwrite(&w_byte, sizeof(uint8_t), 1, f);
+    }
     fclose(f);
     free(path);
 }
 
-void load(room_t **rooms, uint8_t *numrooms, entity_t **player) {
-    /* printf("LOADING:\n"); */
-    uint8_t i;
-    uint16_t rs;
+void load(entity_t **player, uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t *num_rooms, room_t **rooms, uint16_t *num_u_stairs, entity_t **u_stairs, uint16_t *num_d_stairs, entity_t **d_stairs)
+{
+    uint8_t i, j;
+    uint16_t count = 0;
+    uint8_t r_byte;
+    uint16_t r_short;
     uint32_t r;
+    char version[13];
+    version[12] = '\0';
     char *home = getenv("HOME");
     char *gamedir = ".rlg327";
     char *save = "dungeon";
     char *path = malloc(strlen(home) + strlen(gamedir) + strlen(save) + 3);
     sprintf(path, "%s/%s/%s", home, gamedir, save);
     FILE *f = fopen(path, "r");
-    fread(numrooms, sizeof(uint8_t), 1, f);
-    *rooms = malloc(sizeof(room_t) * *numrooms);
-    for (i = 0; i < *numrooms; i++) {
-        fread(&r, sizeof(uint32_t), 1, f);
-        r = be32toh(r);
-        (*rooms)[i].ysize = r % 0x100;
-        (*rooms)[i].xsize = (r >> 8) % 0x100;
-        (*rooms)[i].ypos = (r >> 16) % 0x100;
-        (*rooms)[i].xpos = (r >> 24) % 0x100;
-        /*
-        printf("%x\n", r);
-        printf("%d : %d : %d : %d\n", (*rooms)[i].xpos, (*rooms)[i].ypos, (*rooms)[i].xsize, (*rooms)[i].ysize);
-        */
-    }
+    fread(version, sizeof(char), 12, f);
+    fread(&r, sizeof(uint32_t), 1, f);
+    fread(&r, sizeof(uint32_t), 1, f);
     *player = malloc(sizeof(entity_t));
-    fread(&rs, sizeof(uint16_t), 1, f);
-    rs = be16toh(rs);
-    /* printf("%x\n", rs); */
-    (*player)->ypos = rs % 0x100;
-    (*player)->xpos = (rs >> 8) % 0x100;
+    fread(&r_byte, sizeof(uint8_t), 1, f);
+    (*player)->xpos = r_byte;
+    fread(&r_byte, sizeof(uint8_t), 1, f);
+    (*player)->ypos = r_byte;
     (*player)->symbol = '@';
+    for (i = 0; i < W_HEIGHT; i++) {
+        for (j = 0; j < W_WIDTH; j++) {
+            fread(&r_byte, sizeof(uint8_t), 1, f);
+            hardness[i][j] = r_byte;
+            count++;
+        }
+    }
+    fread(&r_short, sizeof(uint16_t), 1, f);
+    *num_rooms = be16toh(r_short);
+    *rooms = malloc(sizeof(room_t) * *num_rooms);
+    for (i = 0; i < *num_rooms; i++) {
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*rooms)[i].xpos = r_byte;
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*rooms)[i].ypos = r_byte;
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*rooms)[i].xsize = r_byte;
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*rooms)[i].ysize = r_byte;
+    }
+    fread(&r_short, sizeof(uint16_t), 1, f);
+    *num_u_stairs = be16toh(r_short);
+    *u_stairs = malloc(sizeof(entity_t) * *num_u_stairs);
+    for (i = 0; i < *num_u_stairs; i++) {
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*u_stairs)[i].xpos = r_byte;
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*u_stairs)[i].ypos = r_byte;
+        (*u_stairs)[i].symbol = '<';
+    }
+    fread(&r_short, sizeof(uint16_t), 1, f);
+    *num_d_stairs = be16toh(r_short);
+    *d_stairs = malloc(sizeof(entity_t) * *num_d_stairs);
+    for (i = 0; i < *num_d_stairs; i++) {
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*d_stairs)[i].xpos = r_byte;
+        fread(&r_byte, sizeof(uint8_t), 1, f);
+        (*d_stairs)[i].ypos = r_byte;
+        (*d_stairs)[i].symbol = '>';
+    }
     fclose(f);
     free(path);
 }
 
-void maprooms(char map[W_HEIGHT][W_WIDTH], room_t *rooms, uint8_t numrooms) {
+void map_rooms(char map[W_HEIGHT][W_WIDTH], uint8_t hardness[W_HEIGHT][W_WIDTH], uint16_t num_rooms, room_t *rooms)
+{
     uint8_t i, j, k;
-    for (i = 0; i < numrooms; i++) {
+    for (i = 0; i < num_rooms; i++) {
         for (j = 0; j < rooms[i].ysize; j++) {
             for (k = 0; k < rooms[i].xsize; k++) {
                 map[rooms[i].ypos + j][rooms[i].xpos + k] = '.';
@@ -197,33 +288,11 @@ void maprooms(char map[W_HEIGHT][W_WIDTH], room_t *rooms, uint8_t numrooms) {
         }
         /* display[rooms[i].ypos][rooms[i].xpos] = '0' + i; */
     }
-    for (i = 1; i < numrooms; i++) {
-        for (j = rooms[i-1].ypos + rooms[i-1].ysize / 2; j <= rooms[i].ypos + rooms[i].ysize / 2; j++) {
-            if (map[j][rooms[i-1].xpos + rooms[i-1].xsize / 2] == ' ') {
-                map[j][rooms[i-1].xpos + rooms[i-1].xsize / 2] = '#';
-            }
-        }
-        for (j = rooms[i-1].ypos + rooms[i-1].ysize / 2; j >= rooms[i].ypos + rooms[i].ysize / 2; j--) {
-            if (map[j][rooms[i-1].xpos + rooms[i-1].xsize / 2] == ' ') {
-                map[j][rooms[i-1].xpos + rooms[i-1].xsize / 2] = '#';
-            }
-        }
-        for (j = rooms[i-1].xpos + rooms[i-1].xsize / 2; j <= rooms[i].xpos + rooms[i].xsize / 2; j++) {
-            if (map[rooms[i].ypos + rooms[i].ysize / 2][j] == ' ') {
-                map[rooms[i].ypos + rooms[i].ysize / 2][j] = '#';
-            }
-        }
-        for (j = rooms[i-1].xpos + rooms[i-1].xsize / 2; j >= rooms[i].xpos + rooms[i].xsize / 2; j--) {
-            if (map[rooms[i].ypos + rooms[i].ysize / 2][j] == ' ') {
-                map[rooms[i].ypos + rooms[i].ysize / 2][j] = '#';
-            }
-        }
-    }
-    map[rooms[1].ypos + 1][rooms[1].xpos + 1] = '>';
-    map[rooms[numrooms-1].ypos + 1][rooms[numrooms-1].xpos + 1] = '<';
+    //TODO MAP CORRODORS
 }
 
-void initmap(char map[W_HEIGHT][W_WIDTH]) {
+void init_map(char map[W_HEIGHT][W_WIDTH])
+{
     uint8_t i, j;
     for (i = 0; i < W_HEIGHT; i++) {
         for (j = 0; j < W_WIDTH; j++) {
@@ -232,7 +301,8 @@ void initmap(char map[W_HEIGHT][W_WIDTH]) {
     }
 }
 
-void sketchmap(char display[W_HEIGHT][W_WIDTH], char map[W_HEIGHT][W_WIDTH]) {
+void sketch_map(char display[W_HEIGHT][W_WIDTH], char map[W_HEIGHT][W_WIDTH])
+{
     uint8_t i, j;
     for (i = 0; i < W_HEIGHT; i++) {
         for (j = 0; j < W_WIDTH; j++) {
@@ -241,14 +311,16 @@ void sketchmap(char display[W_HEIGHT][W_WIDTH], char map[W_HEIGHT][W_WIDTH]) {
     }
 }
 
-void place(char display[W_HEIGHT][W_WIDTH], entity_t *entities, uint8_t count) {
+void place(char display[W_HEIGHT][W_WIDTH], entity_t *entities, uint16_t count)
+{
     uint8_t i;
     for (i = 0; i < count; i++) {
         display[entities[i].ypos][entities[i].xpos] = entities[i].symbol;
     }
 }
 
-void draw(char display[W_HEIGHT][W_WIDTH]) {
+void draw(char display[W_HEIGHT][W_WIDTH])
+{
     uint8_t i, j;
     for (i = 0; i < W_HEIGHT; i++) {
         for (j = 0; j < W_WIDTH; j++) {
